0f4485d6ec2dac50535a89d67bbb5ef232eb08fc
Firedog Communications <development@firedogpr.com>
2017-01-31 10:05:14 -0500

    String tokenizer

---
+++ MutantX12/src/Strings/Tokenizer.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Firedog on 2017-01-31.
+ */
+public class Tokenizer {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        in.close();
+        //demiliter with space and other symbols
+        String delimiter = "[ !,?._'@]+";
+        StringTokenizer tokenizer = new StringTokenizer(line, delimiter);
+        System.out.println(tokenizer.countTokens());
+        while(tokenizer.hasMoreTokens()){
+            System.out.println(tokenizer.nextToken());
+        }
+
+        //Another implementation would to to split the string using the delimiters
+//            String[] tokens = (line.trim().split(delimiter));
+//            System.out.println(tokens.length);
+//            for (int x = 0; x < tokens.length; x++) {
+//                System.out.println(tokens[x]);
+//            }
+    }
+}
