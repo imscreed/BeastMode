ec5a12553829dc8cabd5c2d325e1ceea945267b4
Mohammed Ibrahim <imscreed@icloud.com>
2017-01-31 23:28:39 -0500

    Added Arrays package with Hourglass problem

---
+++ MutantX12/src/Arrays/SumOfHourGlass2D.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Ibrahim on 2017-01-31.
+ */
+//Calculate and print the highest sum of the hourglasses in a 6x6 array
+public class SumOfHourGlass2D {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        List hourglasses = new ArrayList<Integer>();
+        for(int i=0; i < 4; i++){
+            for(int j=0; j < 4; j++){
+                hourglasses.add(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
+            }
+        }
+        System.out.println(hourglasses);
+        System.out.println(Collections.max(hourglasses));
+    }
+}
+
